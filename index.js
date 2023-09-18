5512d44aed493422fc85c63406ec2945bc4fe5e0
Miguel Rosa <miguelin887@hotmail.com>
2023-09-18 23:35:37 +0200

    finished project

--- index.js
+++ index.js
@@ -16,15 +16,54 @@ let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
 
-function buildTile(color) {
+const buildTile = (color) => {
   const element = document.createElement("div");
   element.classList.add("tile");
   element.setAttribute("data-color", color);
+  element.setAttribute("data-revealed", "false");
+
+  element.addEventListener("click", () => {
+    const revealed = element.getAttribute("data-revealed");
+    if (awaitingEndOfMove || revealed === "true" || element === activeTile) {
+      return;
+    }
+
+    element.style.backgroundColor = color;
+    if (!activeTile) {
+      activeTile = element;
+      return;
+    }
+    const colorToMatch = activeTile.getAttribute("data-color");
+
+    if (colorToMatch === color) {
+      activeTile.setAttribute("data-revealed", "true");
+      element.setAttribute("data-revealed", "true");
+
+      awaitingEndOfMove = false;
+      activeTile = null;
+      revealedCount += 2;
+
+      if (revealedCount === tileCount) {
+        alert("¡You win! Refresh to play again.");
+      }
+      return;
+    }
+
+    awaitingEndOfMove = true;
+
+    setTimeout(() => {
+      element.style.backgroundColor = null;
+      activeTile.style.backgroundColor = null;
+
+      awaitingEndOfMove = false;
+      activeTile = null;
+    }, 1000);
+  });
 
   return element;
-}
+};
 
-for (let i = 0; i > tileCount; i++) {
+for (let i = 0; i < tileCount; i++) {
   const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
   const color = colorsPicklist[randomIndex];
   const tile = buildTile(color);
